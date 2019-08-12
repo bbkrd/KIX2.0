3250437b7b775b102b7c732a1033f2ae5cec0428
Thomas Witthohn <thomas.witthohn@bundesbank.de>
2019-08-12 10:51:23 +0200

    Version 1.0 KIX after API change of KIX (now KIX-UI) Added help files

---
+++ src/main/java/de/bundesbank/jdemetra/kix/annualchaining/docs/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "AnnualChaining-hs.xml", position = 20100)
+package de.bundesbank.jdemetra.kix.annualchaining.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
